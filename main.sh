116e3d7641fd872180c1c3b669cfe68b6953e3a0
Abrasam <samuel.sully@live.com>
2016-06-09 16:53:56 +0100

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "===================="
+python setAirborneGPS.py
+echo "===================="
+while true; do
+        ID="`head -n 1 count.txt`"
+        python increment.py
+        Var="`find ~/images/unsent -printf '%s %p\n'|sort -nr|head -n 1`"
+        String=${Var#*unsent/}
+        echo $String
+        convert -resize 128x96 "/home/pi/images/unsent/$String" /home/pi/images$
+        ./ssdv -e -c SKIPI -i "$ID"  /home/pi/images/temp/temp.jpg output.bin
+        python master.py
+        mv "/home/pi/images/unsent/$String" "/home/pi/images/sent/$String"
+        sleep 0.5
+done
